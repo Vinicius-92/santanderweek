a102663ff0b6212e2e06cf6dca81b4cdd5ab605e
Vinicius Augusto <vinicius_augusto_@msn.com>
2021-05-25 21:10:37 -0300

    Backend project created

---
+++ santander-backend/src/main/java/com/viniciusaugusto/santanderbackend/SantanderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.viniciusaugusto.santanderbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderBackendApplication.class, args);
+	}
+
+}
---
+++ santander-backend/src/test/java/com/viniciusaugusto/santanderbackend/SantanderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciusaugusto.santanderbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
