2fcdebdb194b060edd89bb820e25e2642d69b9c8
Vinicius Augusto <vinicius_augusto_@msn.com>
2021-05-26 23:51:26 -0300

    Config: tsconfig, add models and starting to develop stock-card

---
+++ santander-frontend/src/app/shared/models/stock-model.ts
@@ -0,0 +1,11 @@
+class Stock {
+    id: number;
+    name: string;
+    price: number;
+    minimum: number;
+    exclusiveMininum: boolean;
+    variation: number;
+    date: string;
+}
+
+export default Stock;
